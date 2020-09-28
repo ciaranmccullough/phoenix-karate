2d75dfd644bf18aa6c36f1562e69909810e00fdc
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2020-09-28 23:54:33 +0100

    added new plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
