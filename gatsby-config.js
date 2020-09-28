7dd9ff690b8febde4fd69e57d9a26cb9000b8e85
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2020-09-28 23:35:36 +0100

    fixed image rendering, addded auto trigger deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,17 +3,44 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config()
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Phoenix Karate`,
+    description: `Karate With Purpose.`,
+    author: `Ciaran McCullough`,
+  },
   plugins: [
     `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `gatsby-starter-default`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#e3cce2`,
+    //     theme_color: `#123052`,
+    //     display: `minimal-ui`,
+    //     icon: `${__dirname}/src/images/favicon.png`,
+    //   },
+    // },
     {
       resolve: `gatsby-source-graphql`,
       options: {
         typeName: `WPGraphQL`,
         fieldName: `wpgraphql`,
-        url: `http://phoenixkarate.net/graphql`,
+        url: `${process.env.URL}/graphql`,
       },
     },
   ],
