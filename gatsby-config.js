6e8f57127680c6cdce30e0a0acaf0f454277791c
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2020-09-29 00:06:27 +0100

    fix auto deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,40 +3,10 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-// require("dotenv").config({
-//   path: `.env.${process.env.NODE_ENV}`,
-// })
 
 module.exports = {
-  siteMetadata: {
-    title: `Phoenix Karate`,
-    description: `Karate With Purpose.`,
-    author: `Ciaran McCullough`,
-  },
   plugins: [
     `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#e3cce2`,
-    //     theme_color: `#123052`,
-    //     display: `minimal-ui`,
-    //     icon: `${__dirname}/src/images/favicon.png`,
-    //   },
-    // },
     {
       resolve: `gatsby-source-graphql`,
       options: {
