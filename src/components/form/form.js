123f53d02b4ac95197f7c4f20d2c4f9879391d73
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2020-10-20 20:20:05 +0100

    form testing

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -294,7 +294,7 @@ const BookingForm = () => {
           <Form
             name="contact-demo"
             // method="post"
-            data-netlify="true"
+            data-netlify={true}
             // data-netlify-honeypot="bot-field"
           >
             <input type="hidden" name="form-name" value="contact-demo" />
